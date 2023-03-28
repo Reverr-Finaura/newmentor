1ff9671070962e7ca61c64a0d99705cb45388758
eransh1 <eranshbansal@gmail.com>
2023-03-28 20:33:23 +0530

    add userDoc Slice

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import phnSidebarReducer from "../features/phnSidebarSlice";
+import userDocReducer from "../features/userDocSlice"
 
 export const store = configureStore({
   reducer: {
     phnSidebar: phnSidebarReducer,
+    userDoc:userDocReducer,
   },
 });
