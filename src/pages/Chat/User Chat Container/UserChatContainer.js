6af8c91c72cc47170d05db8f593bccf064d93f85
eransh1 <eranshbansal@gmail.com>
2023-04-03 16:18:38 +0530

    update chat cont

--- src/pages/Chat/User Chat Container/UserChatContainer.js
+++ src/pages/Chat/User Chat Container/UserChatContainer.js
@@ -21,8 +21,8 @@ useEffect(()=>{
 
 {chatData.selectedUserData.map((chat,idx)=>{
   return <>
-  <div key={idx} style={{alignSelf:chat.sendBy===currentcUser.email?"":"end"}} className={styles.chatCont}>
-    <img style={{order:chat.sendBy===currentcUser.email?"":"2"}} className={styles.userImg} src={chat.sendBy===currentcUser.email?currentcUser.image:chatData.selectedUser.userImg} alt="userImg" />
+  <div key={idx} style={{alignSelf:chat.sendBy===currentcUser.email?"end":""}} className={styles.chatCont}>
+    <img style={{order:chat.sendBy===currentcUser.email?"2":""}} className={styles.userImg} src={chat.sendBy===currentcUser.email?currentcUser.image:chatData.selectedUser.userImg} alt="userImg" />
 
     <div className={styles.messNTimeCont}>
       {chat.type?chat.type==="image/png"?<img className={styles.chatImg} src={chat.msg} alt="ima" />: <p>{chat.msg}</p>: <p>{chat.msg}</p>}
